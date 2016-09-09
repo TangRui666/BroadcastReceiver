4147a3d0df775b32f1637b6b99ab46138456e97a
TangRui666 <654427660@qq.com>
2016-09-09 14:35:12 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/broadcastreceiver/MainActivity.java
@@ -0,0 +1,32 @@
+package cn.edu.bistu.cs.se.broadcastreceiver;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        Button button=(Button)findViewById(R.id.buttonSend);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.intent.action.MY_BROADCAST");
+                intent.putExtra("broadcastmsg", "hello receiver.");
+                sendBroadcast(intent);
+
+            }
+        });
+    }
+
+}
